711b28244230bd27cda4d454bfc504af61e66794
icimence <icimence@outlook.com>
2020-04-20 17:52:57 +0800

    feat: pullDownRefresh

--- pages/login/login.js
+++ pages/login/login.js
@@ -44,6 +44,15 @@ Page({
     }
    });
   },
+
+  onPullDownRefresh:function (){
+    wx.showNavigationBarLoading()
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+      }, 1500);
+  },
   
   bindGetUserInfo: function(e) {
    if (e.detail.userInfo) {
@@ -63,9 +72,9 @@ Page({
     //用户按了拒绝按钮
     wx.showModal({
      title: '警告',
-     content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+     content: '您点击了拒绝授权，小程序需要量身定制您，拒绝授权将导致无法进入。',
      showCancel: false,
-     confirmText: '返回授权',
+     confirmText: '返回授权页面',
      success: function(res) {
       // 用户没有授权成功，不需要改变 isHide 的值
       if (res.confirm) {
