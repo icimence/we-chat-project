31c7b24c6d28781f51d22189c26fe0e406330af0
NandoDu <181250028@smail.nju.edu.cn>
2020-05-02 00:29:27 +0800

    dev: calculate init

--- backend/API/views.py
+++ backend/API/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def backend(request):
+    formula = request.GET['formula']
+    try:
+        result = eval(formula, {})
+    except:
+        result = 'Error formula'
+    return HttpResponse(result)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'API.apps.ApiConfig',
+    'API',
 ]
 
 MIDDLEWARE = [
