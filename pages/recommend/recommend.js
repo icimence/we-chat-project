a21557f833a82422a134a42f951b1a4a1b81544a
NandoDu <181250028@smail.nju.edu.cn>
2020-06-01 20:45:28 +0800

    fix recommend show problem

--- pages/recommend/recommend.js
+++ pages/recommend/recommend.js
@@ -29,7 +29,10 @@ Page({
         majorCache: [],
         collectionCache: [],
         newdate: "2020-12-25",
-        nowtime: util.formatTime(new Date()).substr(0, 11)
+        nowtime: util.formatTime(new Date()).substr(0, 11),
+
+        freshStatus: 'more', // 当前刷新的状态
+        showRefresh: false   // 是否显示下拉刷新组件
     },
 
     onLoad: function (options) {
@@ -458,5 +461,53 @@ Page({
         }
       }
     })
-  }
+  },
+  // 触摸开始
+  touchStart(e) {
+    this.setData({
+      startY: e.changedTouches[0].pageY,
+      freshStatus: 'more'
+    })
+  },
+  // 触摸移动
+  touchMove(e) {
+    let endY = e.changedTouches[0].pageY;
+    let startY = this.data.startY;
+    let dis = endY - startY;
+    // 判断是否下拉
+    if (dis <= 0) {
+      return;
+    }
+    let offsetTop = e.currentTarget.offsetTop;
+    if (dis > 20) {
+      this.setData({
+        showRefresh: true
+      }, () => {
+        if (dis > 50) {
+          this.setData({
+            freshStatus: 'end'
+          })
+        } else {
+          this.setData({
+            freshStatus: 'more'
+          })
+        }
+      })
+    } else {
+      this.setData({
+        showRefresh: false
+      })
+    }
+  },
+  // 触摸结束
+  touchEnd(e) {
+    if (this.data.freshStatus == 'end') {
+      // 延迟 500 毫秒，显示 “刷新中”，防止请求速度过快不显示
+      this.onPullDownRefresh()
+    } else {
+      this.setData({
+        showRefresh: false
+      })
+    }
+  },
 })
